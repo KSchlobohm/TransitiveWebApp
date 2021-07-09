82e8155d25fb7eb47a370ea1272205ebc8f178be
Ken Schlobohm <keschlob@microsoft.com>
2021-07-09 11:12:51 -0500

    added empty ASP.NET Core and changed class libraries to netstandard. ASP.NET full framework gives runtime error for Serilog.

---
+++ TransitiveWebApp.Core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TransitiveWebApp.Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
